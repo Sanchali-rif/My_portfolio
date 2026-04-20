f6aade500c79594676eea334142cf5278f0b4b5a
Sanchali-rif <sanchalisaha05@gmail.com>
2026-04-20 17:14:30 +0530

    overall changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -94,7 +94,8 @@ const Project = () => {
                 transition: 'all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1)',
                 alignSelf: 'flex-start',
                 textDecoration: 'none',
-                marginTop: '0.2rem'
+                marginTop: '0.2rem',
+                cursor: 'pointer'
               }}
               onMouseEnter={(e) => {
                 e.currentTarget.style.background = 'var(--c-yellow)';
